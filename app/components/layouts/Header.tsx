3eb940acad27d5632baf8d230dc2122216e008a6
barbie-d-oll <dollarbynarhbarbie@gmail.com>
2026-07-02 12:44:19 +0000

    Create dashboard header

---
+++ app/components/layouts/Header.tsx
@@ -0,0 +1,52 @@
+import { Bell, Search } from "lucide-react";
+
+export default function Header() {
+    return (
+        <header className="flex items-center justify-between bg-white rounded-xl p-6 shadow-sm">
+
+            {/* Left Side */}
+            <div>
+                <h1 className="text-3xl font-bold text-slate-800">
+                    Dashboard
+                </h1>
+
+                <p className="text-gray-500">
+                    Welcome back, Barbara 👋
+                </p>
+            </div>
+
+            {/* Right Side */}
+            <div className="flex items-center gap-4">
+
+                {/* Search Box */}
+                <div className="flex items-center bg-gray-100 px-4 py-2 rounded-lg">
+                    <Search size={18} className="text-gray-500" />
+
+                    <input
+                        type="text"
+                        placeholder="Search..."
+                        className="bg-transparent outline-none ml-2"
+                    />
+                </div>
+
+                {/* Notification */}
+                <button className="bg-gray-100 p-3 rounded-lg">
+                    <Bell size={20} />
+                </button>
+
+                {/* User */}
+                <div className="text-right">
+                    <h3 className="font-semibold">
+                        Barbara Logah
+                    </h3>
+
+                    <p className="text-sm text-gray-500">
+                        Company Administrator
+                    </p>
+                </div>
+
+            </div>
+
+        </header>
+    );
+}
---
+++ app/components/layouts/Sidebar.tsx
@@ -0,0 +1,29 @@
+export default function Sidebar() {
+    return (
+        <div>
+            <h1 className="w-64 h-screen bg-slate-900 text-white p-6">Digital Visitor Log</h1>
+            <p>MAIN MENU</p>
+            <ul>
+                <li>Dashboard</li>
+
+                <li>Visitors</li>
+
+                <li>Attendance</li>
+
+                <li>Reports</li>
+
+                <li>Notifications</li>
+
+                <li>Settings</li>
+
+            </ul>
+
+
+            <hr />
+
+            <p>Barbara Logah</p>
+            <p>Administrator</p>
+            <button>Logout</button>
+        </div>
+    );
+}
